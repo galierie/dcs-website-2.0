b3a8f6a5ea63d022cd43a5040d0ebc583d935ed1
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-25 20:42:43 +0800

    feat(NavBar): add all webpage links

---
+++ website-frontend/src/routes/NavBar.svelte
@@ -0,0 +1,56 @@
+<div class="w-full flex justify-center">
+    <nav class="w-4/5 sticky flex justify-between">
+        <a href="/"><p>logo</p></a>
+        <ul class="w-4/5 flex justify-center gap-8">
+             <li>
+                <a href="/about">About</a>
+                <ul class="">
+                    <li><a href="/about/overview">Overview</a></li>
+                    <li><a href="/about/history">History</a></li>
+                </ul>
+            </li>
+            <li><a href="/events">Events</a></li>
+            <li><a href="/people">People</a></li>
+            <li>
+                <a href="/academics">Academics</a>
+                <ul>
+                    <li><a href="/academics/undergraduate">Undergraduate Program</a></li>
+                    <li><a href="/academics/graduate">Graduate Program</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="/research">Research</a>
+                <ul>
+                    <li><a href="/research/overview">Overview</a></li>
+                    <li><a href="/research/outputs">Featured Outputs</a></li>
+                    <li>
+                        <a href="/research/labs">Labs</a>
+                        <ul>
+                            <li><a href="/research/labs/ndsl">Networks and Distributed Systems Laboratory</a></li>
+                            <li><a href="/research/labs/cvmil">Computer Vision and Machine Intelligence Laboratory</a></li>
+                            <li><a href="/research/labs/s3">Service Science and Software Engineering Laboratory</a></li>
+                            <li><a href="/research/labs/acl">Algorithms and Complexity Laboratory</a></li>
+                            <li><a href="/research/labs/lcl">Logic and smth here Laboratory</a></li>
+                            <li><a href="/research/labs/wsl">Web Science Laboratory</a></li>
+                            <li><a href="/research/labs/csl">Computer Security Laboratory</a></li>
+                            <li><a href="/research/labs/smsl">System Modeling and Simulation Laboratory</a></li>
+                            <li><a href="/research/labs/scl">Scientific Computing Laboratory</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="/students">Students</a>
+                <ul>
+                    <li><a href="/students/life">Organizations</a></li>
+                    <li><a href="/students/resources">Resources</a></li>
+                </ul>
+            </li>
+            <li><a href="/alumni">Alumni</a></li>
+            <li>
+                <a href="/linkages">Linkages</a>
+                <ul><li><a href="/linkages/partnerships">Partnerships</a></li></ul>
+            </li>
+        </ul>
+    </nav>
+</div>
