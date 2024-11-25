684d984b9f992880fbaf47463ad58731c3b0b46b
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-25 20:44:06 +0800

    feat(NavBar): make component for navbar items

---
+++ website-frontend/src/lib/components/nav/NavItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { slide } from "svelte/transition";
+    export let href: string, to: string;
+
+    let show = false;
+</script>
+
+<li 
+    class="relative"
+    on:pointerenter={() => {show = true}} 
+    on:pointerleave={() => {show = false}}
+>
+    <a {href}>{to}</a>
+    {#if show}
+        <ul
+            class="absolute w-40" 
+            transition:slide
+        >
+            <slot />
+        </ul>
+    {/if}
+</li>
