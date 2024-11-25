dc49a1ae6d8e51543219820d48dbdd4be556be64
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-25 21:59:21 +0800

    style(NavBar): add styles to navbar

--- website-frontend/src/lib/components/nav/NavItem.svelte
+++ website-frontend/src/lib/components/nav/NavItem.svelte
@@ -6,14 +6,14 @@
 </script>
 
 <li 
-    class="relative"
+    class="relative rounded-sm text-sm transition-colors duration-300 ease-in-out hover:bg-accent"
     on:pointerenter={() => {show = true}} 
     on:pointerleave={() => {show = false}}
 >
-    <a {href}>{to}</a>
+    <a {href} class="px-3 py-1 flex items-center">{to}</a>
     {#if show}
         <ul
-            class="absolute w-40 {position}" 
+            class="absolute w-40 p-0.5 bg-background {position}" 
             transition:slide
         >
             <slot />
