5b61436c87037ef8b72625e793d580edbd6424b3
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-25 20:46:02 +0800

    refactor(NavBar): prefer NavItem component over li tags

--- website-frontend/src/routes/NavBar.svelte
+++ website-frontend/src/routes/NavBar.svelte
@@ -1,56 +1,44 @@
+<script lang="ts">
+    import NavItem from "$lib/components/nav/NavItem.svelte";
+</script>
+
 <div class="w-full flex justify-center">
     <nav class="w-4/5 sticky flex justify-between">
         <a href="/"><p>logo</p></a>
         <ul class="w-4/5 flex justify-center gap-8">
-             <li>
-                <a href="/about">About</a>
-                <ul class="">
-                    <li><a href="/about/overview">Overview</a></li>
-                    <li><a href="/about/history">History</a></li>
-                </ul>
-            </li>
-            <li><a href="/events">Events</a></li>
-            <li><a href="/people">People</a></li>
-            <li>
-                <a href="/academics">Academics</a>
-                <ul>
-                    <li><a href="/academics/undergraduate">Undergraduate Program</a></li>
-                    <li><a href="/academics/graduate">Graduate Program</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="/research">Research</a>
-                <ul>
-                    <li><a href="/research/overview">Overview</a></li>
-                    <li><a href="/research/outputs">Featured Outputs</a></li>
-                    <li>
-                        <a href="/research/labs">Labs</a>
-                        <ul>
-                            <li><a href="/research/labs/ndsl">Networks and Distributed Systems Laboratory</a></li>
-                            <li><a href="/research/labs/cvmil">Computer Vision and Machine Intelligence Laboratory</a></li>
-                            <li><a href="/research/labs/s3">Service Science and Software Engineering Laboratory</a></li>
-                            <li><a href="/research/labs/acl">Algorithms and Complexity Laboratory</a></li>
-                            <li><a href="/research/labs/lcl">Logic and smth here Laboratory</a></li>
-                            <li><a href="/research/labs/wsl">Web Science Laboratory</a></li>
-                            <li><a href="/research/labs/csl">Computer Security Laboratory</a></li>
-                            <li><a href="/research/labs/smsl">System Modeling and Simulation Laboratory</a></li>
-                            <li><a href="/research/labs/scl">Scientific Computing Laboratory</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="/students">Students</a>
-                <ul>
-                    <li><a href="/students/life">Organizations</a></li>
-                    <li><a href="/students/resources">Resources</a></li>
-                </ul>
-            </li>
-            <li><a href="/alumni">Alumni</a></li>
-            <li>
-                <a href="/linkages">Linkages</a>
-                <ul><li><a href="/linkages/partnerships">Partnerships</a></li></ul>
-            </li>
+            <NavItem href="/about" to="About">
+                <NavItem href="/about/overview" to="Overview" />
+                <NavItem href="/about/history" to="History" />
+            </NavItem>
+            <NavItem href="/events" to="Events" />
+            <NavItem href="/people" to="People" />
+            <NavItem href="/academics" to="Academics">
+                <NavItem href="/academics/undergraduate" to="Undergraduate Program" />
+                <NavItem href="/academics/graduate" to="Graduate Program" />
+            </NavItem>
+            <NavItem href="/research" to="Research">
+                <NavItem href="/research/overview" to="Overview" />
+                <NavItem href="/research/labs" to="Laboratories">
+                    <NavItem href="/research/labs/ndsl" to="Networks and Distributed Systems Laboratory" />
+                    <NavItem href="/research/labs/cvmil" to="Computer Vision and Machine Intelligence Laboratory" />
+                    <NavItem href="/research/labs/s3" to="Service Science and Software Engineering Laboratory" />
+                    <NavItem href="/research/labs/acl" to="Algorithms and Complexity Laboratory" />
+                    <NavItem href="/research/labs/lcl" to="Logic and smth here Laboratory" />
+                    <NavItem href="/research/labs/wsl" to="Web Science Laboratory" />
+                    <NavItem href="/research/labs/csl" to="Computer Security Laboratory" />
+                    <NavItem href="/research/labs/smsl" to="System Modeling and Simulation Laboratory" />
+                    <NavItem href="/research/labs/scl" to="Scientific Computing Laboratory" />
+                </NavItem>
+                <NavItem href="/research/outputs" to="Featured Outputs" />
+            </NavItem>
+            <NavItem href="/students" to="Students">
+                <NavItem href="/students/life" to="Organizations" />
+                <NavItem href="/students/resources" to="Resources" />
+            </NavItem>
+            <NavItem href="/alumni" to="Alumni" />
+            <NavItem href="/linkages" to="Linkages">
+                <NavItem href="/linkages/partnerships" to="Partnerships" />
+            </NavItem>
         </ul>
     </nav>
 </div>
