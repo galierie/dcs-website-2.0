c90faa1c5f39232dffa05c0f044178c4ec6a62a0
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-08-22 11:22:28 +0800

    feat: add logging to system

--- website-frontend/src/hooks.server.ts
+++ website-frontend/src/hooks.server.ts
@@ -1,7 +1,34 @@
+import { dev } from '$app/environment';
+import pino from 'pino';
+import pretty, { type PrettyStream } from 'pino-pretty';
+
+let stream: PrettyStream | undefined;
+
+if (dev) {
+	stream = pretty();
+}
+
+const logger = pino({
+	transport: stream ? { target: 'pino-pretty', options: { colorize: true } } : undefined
+});
+
 export async function handle({ event, resolve }) {
+	const { route: { id: route }, getClientAddress } = event;
+
+	logger.info({
+		route,
+		clientAddress: getClientAddress()
+	}, 'handling request')
+
 	return await resolve(event, {
 		filterSerializedResponseHeaders: (key) => {
 			return key.toLowerCase() === 'content-type';
 		}
 	});
+}
+
+export async function handleError({ error }) {
+	logger.error({
+		error
+	}, 'error occurred with code')
 }
